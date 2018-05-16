0af56380a6865e21ba9785dba596ad840c5f3f07
vlad <vlad.miakshin@gmail.com>
2018-05-16 13:07:33 +0300

    writed test for updateUser function

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,21 +1,38 @@
 import Login from '../components/login/Login';
 import React, { Component } from 'react';
-import configureStore from 'redux-mock-store'
+import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../store';
+import * as actions from '../store/redux/user/actions';
 
-const initialState = {output:100}
-const mockStore = configureStore()
-let store,component;
+const user = {
+  login: 'login',
+  name: 'loginName',
+  friends: [],
+  id: '1',
+  confirmPass: '12341234',
+};
 
-beforeEach(()=>{
-    store = mockStore(initialState)
-    component = shallow(<Login store={store} /> )
-})
+const mockStore = configureStore();
+  let component;
 
-describe('Login component', ()=>{
+beforeEach(() => {
+  component = shallow(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+});
+
+describe('Login component', () => {
   it('Login was created and inputs is empty', () => {
     const loginInput = component.find('login');
-    console.log(loginInput)
     expect(loginInput.length).toEqual(0);
-  })
-})
+  });
+
+  it('user in state should be equal recive user', () => {
+    store.dispatch(actions.updateUser(user)).then
+      expect(store.getState().user).toBe(user);
+  });
+});
